87fb7a16da1da46c4210f36a2469bbe876866410
imtahirabatool <btahira183@gmail.com>
2025-07-11 23:48:04 +0500

    update hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -4,7 +4,7 @@ export default ({req})=>{
     if(typeof window === 'undefined'){
         // We are on the server
         return axios.create({
-            baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            baseURL: 'https://www.tixorr.tech',
             headers: req.headers
         });
     }   else{
